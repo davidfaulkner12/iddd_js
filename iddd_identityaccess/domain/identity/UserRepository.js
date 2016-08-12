6e659c1a5b705cefec1e480391880f4eb1228886
David Wang-Faulkner <david@wangfaulkner.com>
2016-08-12 10:40:22 -0400

    Cleaned up copyright info

--- iddd_identityaccess/domain/identity/UserRepository.js
+++ iddd_identityaccess/domain/identity/UserRepository.js
@@ -39,7 +39,7 @@ class UserRepository extends InMemoryRepository {
     aTenantId,
     aUsername,
     anEncryptedPassword) {
-      
+
     return _.find(this.repository, (user) => {
       return  _.isEqual(user.tenantId, aTenantId) &&
         user.username == aUsername &&
@@ -50,70 +50,3 @@ class UserRepository extends InMemoryRepository {
 }
 
 module.exports = UserRepository
-
-/*
-//   Copyright 2012,2013 Vaughn Vernon
-//
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
-//
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//   See the License for the specific language governing permissions and
-//   limitations under the License.
-
-package com.saasovation.identityaccess.infrastructure.persistence;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-
-import com.saasovation.common.persistence.CleanableStore;
-import com.saasovation.identityaccess.domain.model.identity.FullName;
-import com.saasovation.identityaccess.domain.model.identity.TenantId;
-import com.saasovation.identityaccess.domain.model.identity.User;
-import com.saasovation.identityaccess.domain.model.identity.UserRepository;
-
-public class InMemoryUserRepository implements UserRepository, CleanableStore {
-
-
-
-    @Override
-    public User userFromAuthenticCredentials(
-            TenantId aTenantId,
-            String aUsername,
-            String anEncryptedPassword) {
-
-        for (User user : this.repository().values()) {
-            if (user.tenantId().equals(aTenantId)) {
-                if (user.username().equals(aUsername)) {
-                    if (user.internalAccessOnlyEncryptedPassword().equals(anEncryptedPassword)) {
-                        return user;
-                    }
-                }
-            }
-        }
-
-        return null;
-    }
-
-    @Override
-    public User userWithUsername(TenantId aTenantId, String aUsername) {
-        for (User user : this.repository().values()) {
-            if (user.tenantId().equals(aTenantId)) {
-                if (user.username().equals(aUsername)) {
-                    return user;
-                }
-            }
-        }
-
-        return null;
-    }
-
-}
-*/
