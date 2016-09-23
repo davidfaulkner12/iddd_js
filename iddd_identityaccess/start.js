b4ed2a413bdfc839665101c2752b1dc5d415e504
David Wang-Faulkner <david@wangfaulkner.com>
2016-09-23 10:39:57 -0400

    Minor changes to stuff

--- iddd_identityaccess/start.js
+++ iddd_identityaccess/start.js
@@ -1,1 +1,30 @@
 console.log("Starting identity access")
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/hello/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
